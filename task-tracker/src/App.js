7d787c95c76fc29378ab77856f34cef9cb079b52
fatmaadam-lab <adamfatma201@gmail.com>
2022-08-05 04:14:05 +0300

    Add React Icon Add viewing tasks Add delete functionalty

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -24,10 +24,23 @@ function App() {
     },
 
 ])
+
+// Delete Tasks function 
+const deleteTask = (id) => { 
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+
+
+
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={tasks}/>
+      {
+        tasks.length > 0 ?
+        ( <Tasks tasks={tasks} onDelete={deleteTask}/>) :
+        ('No Tasks to Show')
+      }
       
     </div>
   );
